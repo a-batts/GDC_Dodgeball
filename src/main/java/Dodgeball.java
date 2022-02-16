855a5ecf0d6ec67b78cf0428abc6137d490ecd1f
Alex Batts <alex.batts05@gmail.com>
2022-02-16 14:21:39 -0500

    Created initial gameboard and main file

--- src/main/java/Dodgeball.java
+++ src/main/java/Dodgeball.java
@@ -1,3 +1,25 @@
-public class Dodgeball {
-    
+import javax.swing.*;
+import java.awt.EventQueue;
+
+public class Dodgeball extends JFrame {
+
+    public Dodgeball(){
+        setupInterface();
+    }
+
+    public void setupInterface(){
+        add(new Gameboard());
+        setSize(500, 500);
+        setTitle("Cat Dodgeball");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> {
+            Dodgeball game = new Dodgeball();
+            game.setVisible(true);
+        });
+    }
+
 }
---
+++ src/main/java/Gameboard.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Gameboard extends JPanel {
+    public Gameboard(){
+
+    }
+}
